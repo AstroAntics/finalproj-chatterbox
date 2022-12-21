9cd48c089440ed5e664caed49540bba54c3aeda4
AstroAntics <vucinicm@asbindia.org>
2022-12-21 08:40:48 +0200

    everything up to this point

--- client/chatterbox/src/Logo.js
+++ client/chatterbox/src/Logo.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Image, keyframes, usePrefersReducedMotion } from '@chakra-ui/react';
-import logo from './logo.svg';
 
 const spin = keyframes`
   from { transform: rotate(0deg); }
@@ -13,6 +12,4 @@ export const Logo = props => {
   const animation = prefersReducedMotion
     ? undefined
     : `${spin} infinite 20s linear`;
-
-  return <Image animation={animation} src={logo} {...props} />;
 };
